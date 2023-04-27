1fd27194519dea98ecd9b67da9f22c6bf3b7f9a5
Cameron <cameron.morgan@skiff.com>
2023-04-27 04:55:47 -0500

    Completed implementation of 'Display' component tests.

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -3,8 +3,49 @@ import { render, fireEvent, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Display from './../Display';
 
-test('renders without errors with no props', async () => { });
+import { waitFor } from '@testing-library/react';
 
-test('renders Show component when the button is clicked ', () => { });
+// ----- TEST 2 SPY -----
+import mockFetchShow from './../../api/fetchShow';
+jest.mock('./../../api/fetchShow')
 
-test('renders show season options matching your data when the button is clicked', () => { });
+// ----- TEST 1 -----
+test('renders without errors with no props', async () => {
+  render(<Display />)
+});
+
+// ----- TEST 2 -----
+test('renders Show component when the button is clicked ', async () => {
+  mockFetchShow.mockResolvedValueOnce(exampleShowData);
+  render(<Display />);
+  const lookup = screen.getByRole("button");
+  fireEvent.click(lookup);
+  const check = await screen.findByTestId("show-container");
+  expect(check).toBeInTheDocument();
+});
+
+// ----- TEST 3 -----
+test('renders show season options matching your data when the button is clicked', async () => {
+  mockFetchShow.mockResolvedValueOnce(exampleShowData);
+  render(<Display />);
+  const lookup = screen.getByRole("button");
+  fireEvent.click(lookup);
+
+  await waitFor(() => {
+    const options = screen.queryAllByTestId("season-option");
+    expect(options).toHaveLength(5);
+  })
+});
+
+// ----- EXAMPLE SHOW TEST OBJECT -----
+const exampleShowData = {
+  name: "Stranger Things",
+  summary: "A show about kids and monsters.",
+  seasons: [
+    { id: 0, name: "Season 1", episodes: [] },
+    { id: 1, name: "Season 2", episodes: [] },
+    { id: 2, name: "Season 3", episodes: [] },
+    { id: 3, name: "Season 4", episodes: [] },
+    { id: 4, name: "Season 5", episodes: [] },
+  ]
+};

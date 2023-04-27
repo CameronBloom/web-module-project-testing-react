7a1a5594f0166cd59ef6cee102653987187047ac
Cameron <cameron.morgan@skiff.com>
2023-04-26 20:24:00 -0500

    Completed the 'Show' component test implementation.

--- src/components/tests/Show.test.js
+++ src/components/tests/Show.test.js
@@ -3,8 +3,6 @@ import { render, fireEvent, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Show from './../Show';
 
-
-
 test('renders without errors', () => { 
   render(<Show show={ exampleShowData } selectedSeason={ 0 } />)
   const lookup = screen.queryByText(/Season 1/i);
@@ -13,7 +11,7 @@ test('renders without errors', () => {
   expect(lookup).toHaveTextContent("Season 1");
 });
 
-test('renders Loading component when prop show is null', () => { 
+test('renders Loading component when prop show is null', () => {
   render(<Show show={ null }  />)
   const lookup = screen.queryByTestId("loading-container");
   expect(lookup).toBeInTheDocument();
@@ -21,11 +19,30 @@ test('renders Loading component when prop show is null', () => {
   expect(lookup).toHaveTextContent("Fetching data...");
 });
 
-test('renders same number of options seasons are passed in', () => { });
+test('renders same number of options seasons are passed in', () => {
+  render(<Show show={ exampleShowData } selectedSeason={ "none" } />);
+  const lookup = screen.queryAllByTestId("season-option");
+  expect(lookup).toHaveLength(5);
+});
 
-test('handleSelect is called when an season is selected', () => { });
+test('handleSelect is called when an season is selected', () => {
+  const handleSelect = jest.fn();
+  render(<Show show={ exampleShowData } selectedSeason={ "none" } handleSelect={ handleSelect } />);
+  const lookup = screen.queryByLabelText(/Select A Season/i);
+  expect(lookup).toHaveLength(6); // 5 seasons & null
+  fireEvent.change(lookup, ["1"])
+  expect(handleSelect).toBeCalled();
+});
 
-test('component renders when no seasons are selected and when rerenders with a season passed in', () => { });
+test('component renders when no seasons are selected and when rerenders with a season passed in', () => {
+  const { rerender } = render(<Show show={ exampleShowData } selectedSeason={ "none" } />);
+  let lookup = screen.queryByTestId("episodes-container")
+  expect(lookup).not.toBeInTheDocument();
+
+  rerender(<Show show={ exampleShowData } selectedSeason={ 0 } />)
+  lookup = screen.queryByTestId("episodes-container")
+  expect(lookup).toBeInTheDocument();
+});
 
 // ----- EXAMPLE SHOW TEST OBJECT -----
 const exampleShowData = {
